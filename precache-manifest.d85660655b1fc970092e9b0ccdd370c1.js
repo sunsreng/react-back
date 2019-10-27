2fc5d8069c625208e044d5b65533b3b8d41b5b79
“Sun <sun.sreng123@gmail.com>
2019-10-27 22:08:59 +0700

    Updates

---
+++ precache-manifest.d85660655b1fc970092e9b0ccdd370c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d482de4f5f09ae86e4788d64f8a2771c",
+    "url": "/react-back/index.html"
+  },
+  {
+    "revision": "994cb8c57e8130696dee",
+    "url": "/react-back/static/css/main.9f379c67.chunk.css"
+  },
+  {
+    "revision": "451c67a2b457a86f1712",
+    "url": "/react-back/static/js/2.de6ff0f4.chunk.js"
+  },
+  {
+    "revision": "994cb8c57e8130696dee",
+    "url": "/react-back/static/js/main.1ca0bcbc.chunk.js"
+  },
+  {
+    "revision": "7d7ffbe573dc3ad7d963",
+    "url": "/react-back/static/js/runtime-main.2a46b5bd.js"
+  }
+]);
